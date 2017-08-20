5e8dcb097aa45f111bf77a4e77e560ecfbc795b1
BenceZambo <bence.zambo96@gmail.com>
2017-08-21 00:06:10 +0200

    done

---
+++ src/prime/OptimusPrime.java
@@ -0,0 +1,11 @@
+package prime;
+
+public class OptimusPrime {
+    public static void main(String[] args) {
+
+        algorithm algorithmObject = new algorithm();
+
+        System.out.println(algorithmObject.primes(100000));
+
+    }
+}
